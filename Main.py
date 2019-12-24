0dc46d7eab0076585ce419fbb1ec02b81aca37d8
Sekban <semihersahin@gmail.com>
2019-12-24 14:46:15 +0300

    Phase 2

--- Main.py
+++ Main.py
@@ -5,9 +5,8 @@ import numpy as np
 from GreedyApproach import Greedy
 from SimulatedAnnealing import SimulatedAnnealing
 
-data_directory_keywords = ['No_of_Concentrators', 'Initial_Temp_SA', 'Final_Temp_SA', 'No_of_Terminals', 'No_of_Cycles_SA', 'No_of_Iterations_SA']
-test_size_keywords = ['Test_Large', 'Test_Small']
-
+data_directory_keywords = ['No_of_Iterations_SA']
+test_size_keywords = ['Test_9']
 
 if __name__ == "__main__":    
   for data_directory_keyword in data_directory_keywords:
@@ -24,7 +23,7 @@ if __name__ == "__main__":
         terminal_assigment_costs = []
 
         # Parsing the initialsetup.csv which holds the information on number of concentrators, and number of terminals in the network
-        with open('TestData/Phase_1/' + data_directory_keyword + '/' + test_size_keyword + '/initialsetup.csv', encoding='utf8') as csvfile:
+        with open('TestData/Phase_2/' + data_directory_keyword + '/' + test_size_keyword + '/initialsetup.csv', encoding='utf8') as csvfile:
           readCSV = csv.reader(csvfile, delimiter=',')
           'The following line skips the row that includes the headers'
           next(readCSV)
@@ -36,14 +35,14 @@ if __name__ == "__main__":
               initial_temp_sa = float(row[4])
               final_temp_sa = float(row[5])
         # Parsing the concentrator_capacity_initialcost.csv which holds the information on individual concentrator capacities, and deployment costs.
-        with open('TestData/Phase_1/' + data_directory_keyword + '/' + test_size_keyword + '/concentrator_capacity_initialcost.csv', encoding='utf8') as csvfile:
+        with open('TestData/Phase_2/' + data_directory_keyword + '/' + test_size_keyword + '/concentrator_capacity_initialcost.csv', encoding='utf8') as csvfile:
           readCSV = csv.reader(csvfile, delimiter=',')
           next(readCSV)
           for row in readCSV:
                 concentrator_capacities.append(int(row[0]))
                 concentrator_deployment_costs.append(int(row[1]))
         # Parsing the terminal_demand_assigncost.csv which holds the information on individual individual terminal demands, and costs of the links with each one of the concentrators.
-        with open('TestData/Phase_1/' + data_directory_keyword + '/' + test_size_keyword + '/terminal_demand_assigncost.csv', encoding='utf8') as csvfile:
+        with open('TestData/Phase_2/' + data_directory_keyword + '/' + test_size_keyword + '/terminal_demand_assigncost.csv', encoding='utf8') as csvfile:
           readCSV = csv.reader(csvfile, delimiter=',')
           next(readCSV)
           for row in readCSV:
@@ -81,7 +80,7 @@ if __name__ == "__main__":
         greedy_running_time = time.time() - start_time
 
         # Writing to file SA
-        simulated_annealing_file = open('Results/Phase_1/' + data_directory_keyword + '/' + test_size_keyword + '/SimulatedAnnealing.txt', 'w')
+        simulated_annealing_file = open('Results/Phase_2/' + data_directory_keyword + '/' + test_size_keyword + '/SimulatedAnnealing.txt', 'w')
         simulated_annealing_file.write('Algorithm ran on average: %s seconds' % (np.average(sa_running_times)) + '\n' + 
         'Algorithm ran on min: %s seconds' % (min(sa_running_times)) + '\n' + 
         'Algorithm ran on max: %s seconds' % (max(sa_running_times)) + '\n' + 
@@ -93,6 +92,6 @@ if __name__ == "__main__":
         simulated_annealing_file.close()
 
         # Writing to file Greedy
-        greedy_file = open('Results/Phase_1/' + data_directory_keyword + '/' + test_size_keyword + '/Greedy.txt', 'w')
+        greedy_file = open('Results/Phase_2/' + data_directory_keyword + '/' + test_size_keyword + '/Greedy.txt', 'w')
         greedy_file.write('Algorithm ran in: %s seconds' % (greedy_running_time) + '\n' + 'Network Cost:' + '\n' + str(greedy_cost) + '\n' + 'Concentrators Used:'+ '\n' + str(greedy_used) + '\n' + 'Concentrator Capacities:'+ '\n' + str(greedy_concentrator_capacities) + '\n' + 'Terminal-Concentrator Relation: ' + '\n' + str(greedy_solution))
         greedy_file.close()
